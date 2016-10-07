c703e9b0fae1dc5c73595300cfc45e185308a8eb
Pafcholini <nadyaivanova14@gmail.com>
2016-10-07 17:36:14 +0200

    service_manager: fix derp
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- cmds/servicemanager/service_manager.c
+++ cmds/servicemanager/service_manager.c
@@ -128,7 +128,7 @@ static int svc_can_register(const uint16_t *name, size_t name_len, pid_t spid, u
         return 0; /* Don't allow apps to register services */
     }
 
-    return check_mac_perms_from_lookup(spid, perm, str8(name, name_len)) ? 1 : 0;
+return check_mac_perms_from_lookup(spid, uid, perm, str8(name, name_len)) ? 1 : 0;
 }
 
 static int svc_can_list(pid_t spid, uid_t uid)
